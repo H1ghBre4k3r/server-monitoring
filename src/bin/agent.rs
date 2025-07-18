46e98c97541238302dcec73b285bb6a07dab4345
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-07-18 19:50:27 +0200

    feat(agent): add configurable address, port, and secret auth
    Introduce a util module to read AGENT_ADDR, AGENT_PORT, and AGENT_SECRET
    from environment variables with defaults. Update Rocket server to use
    these configs for binding address and port. Add request guard to enforce
    optional secret authentication via X-MONITORING-SECRET header, returning
    401 Unauthorized if the secret is set but not provided or mismatched.
    
    This improves flexibility and security by allowing runtime configuration
    and optional access control for the metrics endpoint.

--- src/bin/agent.rs
+++ src/bin/agent.rs
@@ -1,7 +1,18 @@
-use rocket::{get, launch, routes, serde::json::Json};
+use dotenv::Error;
+use rocket::{
+    Request,
+    figment::Figment,
+    get,
+    http::Status,
+    launch,
+    request::{FromRequest, Outcome},
+    routes,
+    serde::json::Json,
+};
 use server_monitoring::{
     ComponentInformation, ComponentOverview, CpuInformation, CpuOverview, MemoryInformation,
     ServerMetrics, SystemInformation,
+    util::{get_addr, get_port, get_secret},
 };
 use sysinfo::{Components, System};
 use tracing::{error, instrument};
@@ -9,7 +20,7 @@ use tracing_subscriber::{filter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[get("/metrics")]
 #[instrument]
-fn index() -> Json<ServerMetrics> {
+fn metrics(secret: SecretKey) -> Json<ServerMetrics> {
     let mut sys = System::new_all();
     sys.refresh_all();
     std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
@@ -93,9 +104,42 @@ fn init() {
         .init();
 }
 
+fn get_config() -> Figment {
+    rocket::Config::figment()
+        .merge(("port", get_port()))
+        .merge(("address", get_addr()))
+}
+
 #[launch]
 fn rocket() -> _ {
     init();
-    let figment = rocket::Config::figment().merge(("port", 1111));
-    rocket::custom(figment).mount("/", routes![index, ping])
+    let figment = get_config();
+
+    rocket::custom(figment).mount("/", routes![metrics, ping])
+}
+
+#[derive(Debug)]
+struct SecretKey;
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for SecretKey {
+    type Error = ();
+
+    async fn from_request(
+        request: &'r rocket::Request<'_>,
+    ) -> rocket::request::Outcome<Self, Self::Error> {
+        let header = request.headers().get_one("X-MONITORING-SECRET");
+        let secret = get_secret();
+        if let Some(secret) = secret {
+            if let Some(passed_secret) = header
+                && passed_secret == secret
+            {
+                Outcome::Success(SecretKey)
+            } else {
+                Outcome::Error((Status::Unauthorized, ()))
+            }
+        } else {
+            Outcome::Success(SecretKey)
+        }
+    }
 }
